c609adedd233ac11bb4a78d69d8bb1d53ad4588c
TanmayB5 <tanmaymg22kar@gmail.com>
2025-05-12 19:32:49 +0530

    Time: 0 ms (100%), Space: 10 MB (11.7%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        // If lengths are different, they can't be anagrams
-        if (s.size() != t.size()) {
-            return false;
-        }
-        
-        // Create frequency arrays for both strings
-        vector<int> freq(26, 0); // Assuming only lowercase letters
-        
-        // Count frequencies of characters in `s` and `t`
-        for (int i = 0; i < s.size(); ++i) {
-            freq[s[i] - 'a']++;
-            freq[t[i] - 'a']--;
-        }
-        
-        // Check if all frequencies are zero
-        for (int count : freq) {
-            if (count != 0) {
-                return false;
-            }
-        }
-        
-        return true;
+       if(s.size()!=t.size()) {return false;}
+
+       unordered_map<char,int>smap;
+       unordered_map<char,int>tmap;
+       for(int i=0;i<s.size();i++){
+        smap[s[i]]++;
+        tmap[t[i]]++;
+       } 
+       return smap == tmap;
     }
 };
