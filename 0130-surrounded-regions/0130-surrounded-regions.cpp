b1d64b069eba671ae2b09c23e990a1a7fe9a756d
TanmayB5 <tanmaymg22kar@gmail.com>
2025-05-28 00:11:58 +0530

    Time: 0 ms (100%), Space: 13.9 MB (81.98%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    void solve(vector<vector<char>>& board) {
+        int m = board.size();
+        if (m == 0) return;
+        int n = board[0].size();
+
+        // Step 1: Mark all 'O's connected to the border
+        for (int i = 0; i < m; ++i) {
+            if (board[i][0] == 'O') dfs(board, i, 0);
+            if (board[i][n - 1] == 'O') dfs(board, i, n - 1);
+        }
+        for (int j = 0; j < n; ++j) {
+            if (board[0][j] == 'O') dfs(board, 0, j);
+            if (board[m - 1][j] == 'O') dfs(board, m - 1, j);
+        }
+
+        // Step 2: Flip all 'O' to 'X', and 'T' back to 'O'
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (board[i][j] == 'O') board[i][j] = 'X';  // Captured region
+                if (board[i][j] == 'T') board[i][j] = 'O';  // Safe region
+            }
+        }
+    }
+
+private:
+    void dfs(vector<vector<char>>& board, int i, int j) {
+        int m = board.size(), n = board[0].size();
+        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != 'O') return;
+
+        board[i][j] = 'T';  // Temporarily mark as safe
+
+        dfs(board, i + 1, j);
+        dfs(board, i - 1, j);
+        dfs(board, i, j + 1);
+        dfs(board, i, j - 1);
+    }
+};
