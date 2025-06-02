f8000efd5e9ba2a5ffb9679a13f691f40ac3896b
TanmayB5 <tanmaymg22kar@gmail.com>
2025-06-02 23:26:30 +0530

    Time: 0 ms (100%), Space: 9.4 MB (100%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int sum1=0;
-        int sum2=0;
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            if(i%2==0){
-            sum1= sum1 + nums[i];
-            } else {
-                continue;
-            }
+        int prev=0;
+        int curr=0;
+        int next=0;
+
+        for(int i=0;i<nums.size();i++){
+            next=max(prev+nums[i],curr);
+            prev=curr;
+            curr=next;
         }
-        for(int i=1;i<n;i++){
-             if(i%2!=0){
-            sum2= sum2 + nums[i];
-            } else {
-                continue;
-            }
-        }
-        return max(sum1,sum2);
+        return curr;
     }
 };
