4f5edeebb7002aa5d491bc13343a4f14529fdbd5
TanmayB5 <tanmaymg22kar@gmail.com>
2025-06-17 18:59:20 +0530

    Time: 8 ms (42.43%), Space: 11 MB (19.61%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
-    int characterReplacement(std::string s, int k) {
-        unordered_map<char, int> count;
-        int res = 0;
+    int characterReplacement(string s, int k) {
+        unordered_map<char, int> freq;
+        int maxFreq = 0, ans = 0;
+        int i = 0;
 
-        int l = 0, maxf = 0;
-        for (int r = 0; r < s.size(); r++) {
-            count[s[r]]++;
-            maxf = max(maxf, count[s[r]]);
+        for(int j = 0; j < s.size(); ++j) {
+            freq[s[j]]++;
+            maxFreq = max(maxFreq, freq[s[j]]);
 
-            while ((r - l + 1) - maxf > k) {
-                count[s[l]]--;
-                l++;
+            // Window size = j - i + 1
+            // If we need to change more than k characters, shrink the window
+            if((j - i + 1) - maxFreq > k) {
+                freq[s[i]]--;
+                i++;
             }
-            res = max(res, r - l + 1);
+
+            ans = max(ans, j - i + 1);
         }
 
-        return res;
+        return ans;
     }
 };
