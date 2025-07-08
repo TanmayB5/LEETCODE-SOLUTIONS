c404f716aeab849663847af680df97b53b03e2f8
TanmayB5 <tanmaymg22kar@gmail.com>
2025-07-08 11:53:07 +0530

    Time: 1 ms (84.16%), Space: 27.1 MB (81.02%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -9,25 +9,26 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution {
-    int preIdx = 0;
-    int inIdx = 0;
+public:
+    int preidx = 0;
+    int inidx = 0;
 
     TreeNode* dfs(vector<int>& preorder, vector<int>& inorder, int limit) {
-        if (preIdx >= preorder.size()) return nullptr;
-        if (inorder[inIdx] == limit) {
-            inIdx++;
+        if (preidx >= preorder.size()) {
+            return nullptr;
+        }
+        if (inorder[inidx] == limit) {
+            inidx++;
             return nullptr;
         }
 
-        TreeNode* root = new TreeNode(preorder[preIdx++]);
+        TreeNode* root = new TreeNode(preorder[preidx++]);
         root->left = dfs(preorder, inorder, root->val);
         root->right = dfs(preorder, inorder, limit);
         return root;
     }
 
-public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         return dfs(preorder, inorder, INT_MAX);
     }
