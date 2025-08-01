5b52546e4e93b3ab78a8001d5d5f8fae2a1ee377
TanmayB5 <tanmaymg22kar@gmail.com>
2025-08-01 20:16:08 +0530

    Time: 0 ms (100%), Space: 8.5 MB (85.35%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if (x == 0) return 0;
+        int l = 0, r = x;
+        int res = 0;
 
-        int low = 1, high = x;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            long long val = 1LL * mid * mid;  // Safe multiplication
-
-            if (val <= x) {
-                low = mid + 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if ((long long) m * m > x) {
+                r = m - 1;
+            } else if ((long long) m * m < x) {
+                l = m + 1;
+                res = m;
             } else {
-                high = mid - 1;
+                return m;
             }
         }
-        return high;
+
+        return res;
     }
 };
