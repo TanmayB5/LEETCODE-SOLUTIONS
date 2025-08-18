7fd89119ce27640f393157b65781ade2f513a268
TanmayB5 <tanmaymg22kar@gmail.com>
2025-08-19 00:43:29 +0530

    Time: 23 ms (73.26%), Space: 17.8 MB (93.95%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -3,13 +3,18 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         vector<int> dp(amount + 1, amount + 1);
         dp[0] = 0;
-        for (int i = 1; i <= amount; i++) {
+
+        for (int i = 1; i < amount + 1; i++) {
             for (int j = 0; j < coins.size(); j++) {
                 if (coins[j] <= i) {
                     dp[i] = min(dp[i], dp[i - coins[j]] + 1);
                 }
             }
         }
-        return dp[amount] > amount ? -1 : dp[amount];
+        if (dp[amount] > amount) {
+            return -1;
+        } else {
+            return dp[amount];
+        }
     }
 };
