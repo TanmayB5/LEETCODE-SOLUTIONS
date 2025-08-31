e2e053b25226e953ceb1eb2acea84e2a7ae735d0
TanmayB5 <tanmaymg22kar@gmail.com>
2025-08-31 17:37:46 +0530

    Time: 0 ms (100%), Space: 20.6 MB (83.39%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,26 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-         int n = nums.size();
-
-        // Sorting the array using insertion sort
-        for (int i = 0; i <= n - 1; i++) {
-            int j = i;
-            while (j > 0 && nums[j - 1] > nums[j]) {
-                // Swap elements to sort the array
-                int temp = nums[j - 1];
-                nums[j - 1] = nums[j];
-                nums[j] = temp;
-                j--;
-            }
+        int ans = 0;
+        for(int i=0;i<nums.size();i++){
+            ans = ans ^ nums[i];
         }
-
-        // XOR all the elements
-        int xorr = 0;
-        for (int i = 0; i < n; i++) {
-            xorr = xorr ^ nums[i];
-        }
-
-        return xorr;
+        return ans;
     }
 };
