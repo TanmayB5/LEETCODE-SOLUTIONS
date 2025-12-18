b14c2fd1864ffb482b373334a8697ac7727e16d3
TanmayB5 <tanmaymg22kar@gmail.com>
2025-12-18 15:51:50 +0530

    Time: 0 ms (100%), Space: 19.5 MB (61.82%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -1,26 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode dummy(0); 
-        ListNode* node = &dummy; 
+        ListNode* dummy = new ListNode();
+        ListNode* tail = dummy;
 
-        while (list1 && list2) {
-            if (list1->val < list2->val) {
-                node->next = list1;
+        while (list1 != NULL && list2 != NULL) {
+            if (list1->val <= list2->val) {
+                tail->next = list1;
                 list1 = list1->next;
             } else {
-                node->next = list2;
+                tail->next = list2;
                 list2 = list2->next;
             }
-            node = node->next;  // ✅ Corrected: Move `node` forward
+            tail = tail->next;
         }
 
-        if (list1) {
-            node->next = list1;
-        } else {
-            node->next = list2;
-        }
+        // attach remaining nodes
+        if (list1 != NULL) tail->next = list1;
+        else tail->next = list2;
 
-        return dummy.next;  // Returns merged list starting from `dummy.next`
+        return dummy->next;
     }
 };
+
+
+
+
+
+
+
