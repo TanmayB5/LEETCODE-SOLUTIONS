b699ad5eb0daaf24981297b4df4d80085144a42d
TanmayB5 <tanmaymg22kar@gmail.com>
2025-12-22 18:50:52 +0530

    Time: 0 ms (100%), Space: 12.5 MB (48.11%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -4,14 +4,26 @@ public:
         int n = nums.size();
 
         if (n == 1) return 0;
+
         if (nums[0] > nums[1]) return 0;
         if (nums[n-1] > nums[n-2]) return n-1;
 
-        for (int i = 1; i < n - 1; i++) {
-            if (nums[i] > nums[i-1] && nums[i] > nums[i+1]) {
+        for (int i = 1; i <= (n - 2) / 2; i++) {
+
+            if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1])
                 return i;
-            }
+
+            int j = n - 1 - i;
+            if (j != i && nums[j] > nums[j - 1] && nums[j] > nums[j + 1])
+                return j;
         }
+
+        if (n % 2 == 1) {
+            int mid = n / 2;
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
+                return mid;
+        }
+
         return -1;
     }
 };
