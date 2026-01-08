a0889714a5dffa9eaf256f46680c884d42ff51bf
TanmayB5 <tanmaymg22kar@gmail.com>
2026-01-08 16:53:02 +0530

    Time: 0 ms (100%), Space: 23.7 MB (72.25%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -6,26 +6,28 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    int diameterOfBinaryTree(TreeNode* root) {
-        int res=0;
-        dfs(root,res);
-        return res;
-    }
+    int diameter = 0;
 
-private:
-    int dfs(TreeNode* root,int& res){
-        if(root== nullptr){
+    int height(TreeNode* root) {
+        if (root == nullptr)
             return 0;
-        }
-    int lefth = dfs(root->left, res);
-    int righth = dfs(root->right, res);
-    res = max(res, lefth+righth);
-    return 1 + max(lefth,righth);
+
+        int lh = height(root->left);
+        int rh = height(root->right);
+
+        diameter = max(diameter, lh + rh);
+
+        return 1 + max(lh, rh);
     }
 
+    int diameterOfBinaryTree(TreeNode* root) {
+        height(root);
+        return diameter;
+    }
 };
