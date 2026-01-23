9fb1e466cdb62abdd84ac1dabcda6fcecaaf1ae0
TanmayB5 <tanmaymg22kar@gmail.com>
2026-01-23 15:06:47 +0530

    Time: 597 ms (76.92%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,16 +1,5 @@
-SELECT 
-    s.user_id,
-    ROUND(
-        IF(COUNT(c.action) = 0, 0, 
-           SUM(c.action = 'confirmed') / COUNT(c.action)
-        ), 2
-    ) AS confirmation_rate
-FROM 
-    Signups s
-LEFT JOIN 
-    Confirmations c 
-    ON s.user_id = c.user_id
-GROUP BY 
-    s.user_id
-ORDER BY 
-    s.user_id;
+# Write your MySQL query statement below
+SELECT s.user_id , ROUND(IFNULL(AVG(c.action = 'confirmed'), 0), 2)  AS confirmation_rate 
+FROM Signups s LEFT JOIN Confirmations c 
+ON s.user_id = c.user_id 
+GROUP BY s.user_id
