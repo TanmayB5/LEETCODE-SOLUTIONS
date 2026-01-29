0372528be7721727bc5104639fa7457f64bce706
TanmayB5 <tanmaymg22kar@gmail.com>
2026-01-29 23:35:14 +0530

    Time: 168 ms (5.1%), Space: 13.6 MB (14.65%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.py
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.py
@@ -1,10 +1,29 @@
+# class Solution(object):
+#     def maxProductDifference(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+#         nums.sort()
+#         return nums[-1] * nums[-2] - nums[0] * nums[1]
 
+
+import heapq
 
 class Solution(object):
     def maxProductDifference(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        nums.sort()
-        return nums[-1] * nums[-2] - nums[0] * nums[1]
+        # min heap (default)
+        min_heap = nums[:]
+        heapq.heapify(min_heap)
+
+        min1 = heapq.heappop(min_heap)
+        min2 = heapq.heappop(min_heap)
+
+        # max heap using negatives
+        max_heap = [-n for n in nums]
+        heapq.heapify(max_heap)
+
+        max1 = -heapq.heappop(max_heap)
+        max2 = -heapq.heappop(max_heap)
+
+        return (max1 * max2) - (min1 * min2)
