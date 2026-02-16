035ffb59445d13ffea171b5c14f8feda8a112d2b
TanmayB5 <tanmaymg22kar@gmail.com>
2026-02-16 11:38:15 +0530

    Time: 0 ms (100%), Space: 9 MB (38.14%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,21 +2,25 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-        for(int i=0;i<s.size();i++){
-            if(s[i]== '(' || s[i]== '[' || s[i]== '{' ){
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
                 st.push(s[i]);
-            }  else {
-                if(st.empty()) return false;
-                char top = st.top();
-                if(s[i]==')'  && top == '(' ||
-                   s[i]==']'  && top == '[' ||
-                   s[i]=='}'  && top == '{'){
-                   st.pop();}
-                   else {
-                    return false;
-                   }
+            } else {
+                if (st.empty()) return false;
+                if (st.top() == '(') {
+                    if(s[i] != ')')
+                         { return false; } else { st.pop(); }
+                } else if (st.top() == '[') {
+                    if(s[i] != ']')
+                         { return false; } else { st.pop(); }
+                } else if (st.top() == '{') {
+                    if(s[i] != '}')
+                         { return false; } else { st.pop(); }
+                }
             }
+            
         }
-        return(st.empty());
+        return st.empty();
     }
 };
