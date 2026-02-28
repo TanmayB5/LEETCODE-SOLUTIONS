326643e7c7388598db35e02b56cb8cb73c1b7400
TanmayB5 <tanmaymg22kar@gmail.com>
2026-03-01 00:19:16 +0530

    Time: 43 ms (84.48%), Space: 29.1 MB (45.53%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -2,27 +2,36 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> ans;
-        int n = nums.size();
-        int sum = 0;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n-2;i++){
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            int l = i+1;
-            int r = n-1;
+        sort(nums.begin(), nums.end());
 
-            while(l<r){
-                sum = nums[i] + nums[l] + nums[r];
-                if(sum==0){
-                    ans.push_back({nums[i],nums[l],nums[r]});
-                    l++;
-                    r--;
-                    while (l < r && nums[l] == nums[l - 1]) l++;
-                    while (l < r && nums[r] == nums[r + 1]) r--;
+        for (int i = 0; i < nums.size() - 2; i++) {
+
+            // skip duplicate i
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int j = i + 1;
+            int k = nums.size() - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum == 0) {
+                    ans.push_back({nums[i], nums[j], nums[k]});
+
+                    j++;
+                    k--;
+
+                   
+                    while (j < k && nums[j] == nums[j - 1]) j++;
+
+                   
+                    while (j < k && nums[k] == nums[k + 1]) k--;
                 }
-                else if(sum<0){
-                    l++;
-                } else {
-                    r--;
+                else if (sum < 0) {
+                    j++;
+                }
+                else {
+                    k--;
                 }
             }
         }
