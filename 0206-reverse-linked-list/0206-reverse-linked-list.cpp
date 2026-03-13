1faba6759ed60e76b4b650acafd4a43248a2d6d9
TanmayB5 <tanmaymg22kar@gmail.com>
2026-03-13 19:50:40 +0530

    Time: 0 ms (100%), Space: 13.4 MB (72.31%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -13,13 +13,16 @@ public:
     ListNode* reverseList(ListNode* head) {
         ListNode* curr = head;
         ListNode* prev = NULL;
-        ListNode* temp;
-        while(curr != NULL){
-            temp = curr->next;
+        
+
+        while(curr){
+            ListNode* next = curr->next;
             curr->next = prev;
             prev = curr;
-            curr = temp;
+            curr = next;
         }
         return prev;
+    
+
     }
 };
