a78fade8b70f0ea4eb6cc47eb1e456bed34411b5
TanmayB5 <tanmaymg22kar@gmail.com>
2026-03-19 00:20:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -4,21 +4,31 @@ public:
 
         int n = s1.size();
         int m = s2.size();
+        vector<int> c1(26, 0);
+        vector<int> c2(26, 0);
 
-        if(n > m) return false;
+        if (n > m)
+            return false;
 
-        sort(s1.begin(), s1.end());
-
-        for(int i = 0; i <= m-n; i++){
-
-            string s = s2.substr(i, n);
-
-            sort(s.begin(), s.end());
-
-            if(s == s1)
-                return true;
+        for (int i = 0; i < n; i++) {
+            c1[s1[i] - 'a']++;
         }
 
+        for (int j = 0; j < n; j++) {
+            c2[s2[j] - 'a']++;
+        }
+        if (c1 == c2) {
+            return true;
+        }
+
+        for (int i = n; i < m; i++) {
+            c2[s2[i] - 'a']++;
+            c2[s2[i - n] - 'a']--;
+
+            if (c1 == c2) {
+                return true;
+            }
+        }
         return false;
     }
 };
