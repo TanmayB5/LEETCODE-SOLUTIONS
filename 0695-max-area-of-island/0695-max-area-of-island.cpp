2ec79d1597a61cda6a116e8e188886ace9b75307
TanmayB5 <tanmaymg22kar@gmail.com>
2026-03-26 05:59:59 +0530

    Time: 0 ms (100%), Space: 27.3 MB (90.01%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,39 +1,34 @@
 class Solution {
 public:
-    int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int row = grid.size();
-        int col = grid[0].size();
-        vector<vector<bool>> vis(row, vector<bool>(col, false));
-        int maxarea = 0;
+    int dfs(vector<vector<int>>& grid, int r, int c){
+        if(r < 0 || r >= grid.size() || c < 0 || c >= grid[0].size() || grid[r][c] == 0){
+            return 0;
+        }
 
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (!vis[i][j] && grid[i][j] == 1) {
-                    int area = 0;
-                    dfs(grid, vis, i, j, area);
-                    maxarea = max(maxarea, area);
+        grid[r][c] = 0; 
+
+        int area = 1; 
+
+        area += dfs(grid, r-1, c);
+        area += dfs(grid, r, c-1);
+        area += dfs(grid, r+1, c);
+        area += dfs(grid, r, c+1);
+
+        return area;
+    }
+
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int ans = 0;
+
+        for(int i = 0; i < grid.size(); i++){
+            for(int j = 0; j < grid[0].size(); j++){
+                if(grid[i][j] == 1){
+                    int area = dfs(grid, i, j);
+                    ans = max(ans, area);
                 }
             }
         }
-        return maxarea;
-    }
 
-private:
-    void dfs(vector<vector<int>>& grid, vector<vector<bool>>& vis, int i, int j, int& area) {
-        int row = grid.size();
-        int col = grid[0].size();
-        
-        // Boundary and condition checks
-        if (i < 0 || i >= row || j < 0 || j >= col || vis[i][j] || grid[i][j] == 0)
-            return;
-
-        vis[i][j] = true;
-        area++;
-
-        // Explore all 4 directions
-        dfs(grid, vis, i + 1, j, area);
-        dfs(grid, vis, i - 1, j, area);
-        dfs(grid, vis, i, j + 1, area);
-        dfs(grid, vis, i, j - 1, area);
+        return ans;
     }
 };
